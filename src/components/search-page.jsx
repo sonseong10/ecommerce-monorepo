84fe46555ba8ab948eb5d8d7f1b086fd08c5d173
sonseong10 <sonseong10@gmail.com>
2021-05-18 21:12:18 +0900

    🩹 Fix wrapper class location to global

--- src/components/search-page.jsx
+++ src/components/search-page.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/modules/search_page.module.css'
 const SearchPage = () => {
   return (
     <div className={`col-md-3 sm-hidden ${styles.isClose} ${styles.isOpen}`}>
-      <div className={styles.wrapper}>
+      <div className="wrapper">
         <article>
           <h2 className="visually-hidden">Search Form</h2>
           <SearchForm></SearchForm>
