13f55fdb15bc9fa3ca270c453953fa96460bd2c5
sonseong10 <sonseong10@gmail.com>
2021-05-19 02:18:58 +0900

    🚧 Add toggle screen function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles/main.css'
 
 import SideNavigation from './components/side-navigation'
 import SearchPage from './components/search-page'
 import ResultPage from './components/result-page'
+import HomePage from './components/home/home-page'
 
 const App = () => {
+  const [menus, setmenus] = useState('home')
+
+  const handleHomeActive = () => {
+    setmenus('home')
+  }
+
+  const handleSearchActive = () => {
+    setmenus('search')
+  }
+
   return (
     <div className="container">
       <div className="row">
-        <SideNavigation></SideNavigation>
-        <SearchPage></SearchPage>
-        <ResultPage></ResultPage>
+        <SideNavigation
+          handleHomeActive={handleHomeActive}
+          handleSearchActive={handleSearchActive}
+          menus={menus}
+        ></SideNavigation>
+        {menus === 'home' ? (
+          <HomePage></HomePage>
+        ) : (
+          <>
+            <SearchPage></SearchPage>
+            <ResultPage></ResultPage>
+          </>
+        )}
       </div>
     </div>
   )
--- src/components/side-navigation.jsx
+++ src/components/side-navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 
 import Logo from '../logo.svg'
 
@@ -6,57 +6,67 @@ import { BiHomeAlt, BiSearch, BiMenu } from 'react-icons/bi'
 import styles from '../styles/modules/global_header.module.css'
 import buttonStyles from '../styles/modules/buttons.module.css'
 
-const SideNavigation = () => {
-  return (
-    <div className="col-sm-4 col-md-3">
-      <div className="wrapper">
-        <header className={styles.globalHeader}>
-          <div className={styles.snbLeft}>
-            <button
-              className={`${styles.snbIconButton} is-open sm-only`}
-              type="button"
-              aria-label="메뉴 열기 버튼"
-            >
-              <BiMenu />
-            </button>
-            <strong className={styles.logo}>
-              <a href="/">
-                <img className="logo-img" src={Logo} alt="WhoMember" />
+const SideNavigation = memo(
+  ({ menus, handleHomeActive, handleSearchActive }) => {
+    return (
+      <div className="col-sm-4 col-md-3">
+        <div className="wrapper">
+          <header className={styles.globalHeader}>
+            <div className={styles.snbLeft}>
+              <button
+                className={`${styles.snbIconButton} is-open sm-only`}
+                type="button"
+                aria-label="메뉴 열기 버튼"
+              >
+                <BiMenu />
+              </button>
+              <strong className={styles.logo}>
+                <a href="/">
+                  <img className="logo-img" src={Logo} alt="WhoMember" />
+                </a>
+              </strong>
+            </div>
+            <div className={`${styles.snbRight} sm-hidden`}>
+              <nav className="snb">
+                <h2 className="visually-hidden">Side Navigation Bar</h2>
+                <ul className="snb-list">
+                  <li className="snb-item">
+                    <button
+                      className={`${styles.snbItemButton} ${
+                        menus === 'home' ? styles.isActive : ''
+                      }`}
+                      onClick={handleHomeActive}
+                      type="button"
+                    >
+                      <BiHomeAlt className={styles.snbButtonIcon} />
+                      Home
+                    </button>
+                  </li>
+                  <li className="snb-item">
+                    <button
+                      className={`${styles.snbItemButton} 
+                    ${menus === 'search' ? styles.isActive : ''}`}
+                      onClick={handleSearchActive}
+                      type="button"
+                    >
+                      <BiSearch className={styles.snbButtonIcon} />
+                      Search
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+              <a
+                className={`${styles.loginBtn} ${buttonStyles.primaryBtn} ${buttonStyles.baseBtn}`}
+                href="/"
+              >
+                Login &#38; Signup
               </a>
-            </strong>
-          </div>
-          <div className={`${styles.snbRight} sm-hidden`}>
-            <nav className="snb">
-              <h2 className="visually-hidden">Side Navigation Bar</h2>
-              <ul className="snb-list">
-                <li className="snb-item">
-                  <button className={`${styles.snbItemButton}`} type="button">
-                    <BiHomeAlt className={styles.snbButtonIcon} />
-                    Home
-                  </button>
-                </li>
-                <li className="snb-item">
-                  <button
-                    className={`${styles.snbItemButton} ${styles.isActive}`}
-                    type="button"
-                  >
-                    <BiSearch className={styles.snbButtonIcon} />
-                    Search
-                  </button>
-                </li>
-              </ul>
-            </nav>
-            <a
-              className={`${styles.loginBtn} ${buttonStyles.primaryBtn} ${buttonStyles.baseBtn}`}
-              href="/"
-            >
-              Login &#38; Signup
-            </a>
-          </div>
-        </header>
+            </div>
+          </header>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
 export default SideNavigation
