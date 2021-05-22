89629d64f29636beeb48fc56778933748b641c4b
sonseong10 <sonseong10@gmail.com>
2021-05-22 20:32:59 +0900

    🚧 Add main page component

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import styles from '../../styles/modules/home_page.module.css'
 
 const HomePage = ({ handleHomeActive }) => {
   useEffect(() => {
@@ -6,10 +7,18 @@ const HomePage = ({ handleHomeActive }) => {
   })
   return (
     <div className="col-sm-4 col-md-9">
-      <div className="wrapper">
-        <article>orenge</article>
-        <article>blue</article>
-        <article>green</article>
+      <div className={`wrapper ${styles.articleWrap}`}>
+        <div className={styles.articleLeft}>
+          <article className={styles.benner}>
+            <h2 className={styles.articleTitle}>회사정보</h2>
+          </article>
+          <article className={styles.todo}>
+            <h2 className={styles.articleTitle}>오늘할일</h2>
+          </article>
+        </div>
+        <article className={styles.member}>
+          <h2 className={styles.articleTitle}>팀원정보</h2>
+        </article>
       </div>
     </div>
   )
