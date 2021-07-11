1266c3af9b22f90eb15bea4fcae2b43197b5139a
sonseong10 <sonseong10@gmail.com>
2021-07-11 16:07:31 +0900

    🚸 Add team list slider button

--- src/components/team-card/team-list.jsx
+++ src/components/team-card/team-list.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import TeamItem from './team-item'
+
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
 
 import styles from '../../styles/modules/team_card.module.css'
 
 const TeamList = ({ cards, userCard }) => {
+  const containerRef = useRef()
+
   const filterKey = Object.keys(cards).filter((key) =>
     cards[key].team.includes(userCard.team)
   )
 
+  const onPrevScroll = () => {
+    containerRef.current.scrollLeft -= 260
+  }
+
+  const onNextScroll = () => {
+    containerRef.current.scrollLeft += 260
+  }
+
   return (
-    <ul className={styles.teamList}>
-      {filterKey.length &&
-        filterKey.map((key) => (
-          <TeamItem card={cards[key]} key={key}></TeamItem>
-        ))}
-    </ul>
+    <div className={styles.teamListWrapper}>
+      <ul className={styles.teamList} ref={containerRef}>
+        {filterKey.length &&
+          filterKey.map((key) => (
+            <TeamItem card={cards[key]} key={key}></TeamItem>
+          ))}
+      </ul>
+
+      <button
+        className={`${styles.moveBtn} ${styles.prevBtn}`}
+        onClick={onPrevScroll}
+      >
+        <BiChevronLeft />
+        <span className="visually-hidden">preview</span>
+      </button>
+
+      <button
+        className={`${styles.moveBtn} ${styles.nextBtn}`}
+        onClick={onNextScroll}
+      >
+        <BiChevronRight />
+        <span className="visually-hidden">next</span>
+      </button>
+    </div>
   )
 }
 
