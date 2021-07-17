20de609cdd6aaa65a125bf30b585b1592d036e33
sonseong10 <sonseong10@gmail.com>
2021-07-17 22:41:37 +0900

    📦 Add carousel for react-slick

---
+++ src/utils/carousel.js
@@ -0,0 +1,13 @@
+const settings = {
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 600,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  className: 'carousel',
+}
+
+export { settings }
