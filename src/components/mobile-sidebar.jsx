9423e3fad3f30cfcfa2fa5700f0884dde5572aa9
sonseong10 <sonseong10@gmail.com>
2021-07-20 14:38:47 +0900

    💄 Update mobile sidebar styles

--- src/components/mobile-sidebar.jsx
+++ src/components/mobile-sidebar.jsx
@@ -4,8 +4,8 @@ import { useHistory } from 'react-router-dom'
 import { BiUser, BiX, BiMoon } from 'react-icons/bi'
 
 import buttonStyles from '../styles/modules/buttons.module.css'
-import layoutStyles from '../styles/modules/mobile-sidebar.module.css'
-import styles from '../styles/modules/common.module.css'
+import sidebarStyles from '../styles/modules/mobile-sidebar.module.css'
+import commonStyles from '../styles/modules/common.module.css'
 
 const MobileSideBar = ({ onLogout, isCard, isOpen, toggleOpenSideBar }) => {
   const history = useHistory()
@@ -22,8 +22,8 @@ const MobileSideBar = ({ onLogout, isCard, isOpen, toggleOpenSideBar }) => {
 
   return (
     <article
-      className={`sm-only ${layoutStyles.sideBar} ${
-        isOpen && layoutStyles.isActive
+      className={`sm-only ${sidebarStyles.sideBar} ${
+        isOpen && sidebarStyles.isActive
       }`}
     >
       <h2 className="visually-hidden">mobile menu</h2>
@@ -32,57 +32,60 @@ const MobileSideBar = ({ onLogout, isCard, isOpen, toggleOpenSideBar }) => {
         <ul>
           <li>
             <button
-              className={`${buttonStyles.baseBtn} ${styles.toolBtn}`}
+              className={`${buttonStyles.baseBtn} ${commonStyles.toolBtn}`}
               onClick={toggleOpenSideBar}
               type="button"
             >
-              <BiMoon className={styles.toolIcon} />
+              <BiMoon className={commonStyles.toolIcon} />
               Dark Mode
             </button>
           </li>
           <li>
             <button
-              className={`${buttonStyles.baseBtn} ${styles.toolBtn}`}
+              className={`${buttonStyles.baseBtn} ${commonStyles.toolBtn}`}
               onClick={goToUpdate}
               type="button"
             >
-              <BiUser className={styles.toolIcon} />
+              <BiUser className={commonStyles.toolIcon} />
 
               {isCard ? 'Info Update' : 'Disable'}
             </button>
           </li>
           <li>
             <button
-              className={`${buttonStyles.baseBtn} ${styles.toolBtn} ${styles.logoutBtn}`}
+              className={`${buttonStyles.baseBtn} ${commonStyles.toolBtn} ${commonStyles.logoutBtn}`}
               type="button"
               onClick={handleLogout}
             >
-              <BiX className={styles.toolIcon} />
+              <BiX className={commonStyles.toolIcon} />
               Log out
             </button>
           </li>
         </ul>
       </nav>
-      <dl className={styles.detailList}>
-        <div className={styles.detailItem}>
-          <dt className="visually-hidden">made</dt>
-          <dd>@July 2021</dd>
-        </div>
-        <div className={styles.detailItem}>
-          <dt className="visually-hidden">github</dt>
-          <dd>
-            <address>
-              <button
-                onClick={() => {
-                  window.open('https://github.com/sonseong10', '_blank')
-                }}
-              >
-                sonseong10
-              </button>
-            </address>
-          </dd>
-        </div>
-      </dl>
+      <footer>
+        <dl className={sidebarStyles.detailList}>
+          <div className={sidebarStyles.detailItem}>
+            <dt className="visually-hidden">made</dt>
+            <dd>@July 2021</dd>
+          </div>
+          <div className={sidebarStyles.detailItem}>
+            <dt className="visually-hidden">github</dt>
+            <dd>
+              <address>
+                <button
+                  onClick={() => {
+                    window.open('https://github.com/sonseong10', '_blank')
+                  }}
+                  type="button"
+                >
+                  sonseong10
+                </button>
+              </address>
+            </dd>
+          </div>
+        </dl>
+      </footer>
     </article>
   )
 }
