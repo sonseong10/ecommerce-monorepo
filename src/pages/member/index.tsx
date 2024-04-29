ca85f24c43a198d5f78632b0632fbd8945e900dd
sonseongyeol <sonseong10@gmail.com>
2024-04-29 14:13:28 +0900

    사용자 정보수정 라우터 추가

--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -6,6 +6,7 @@ import NotFound from 'pages/errors/not-found'
 
 const MemberManage = lazy(() => import('./manage'))
 const Detail = lazy(() => import('./detail/detail'))
+const MemberRegister = lazy(() => import('./register'))
 
 const MemberRoute = () => {
   return (
@@ -23,6 +24,14 @@ const MemberRoute = () => {
         element={
           <Suspense fallback={<Spinner />}>
             <Detail />
+          </Suspense>
+        }
+      />
+      <Route
+        path="register/*"
+        element={
+          <Suspense fallback={<Spinner />}>
+            <MemberRegister />
           </Suspense>
         }
       />
