6f4348c04d5d0412dc1a3798d6430aa4188973fc
sonseongyeol <sonseong10@gmail.com>
2024-05-13 16:33:38 +0900

    진열관리 상시보이게 표시

--- src/pages/display/index.tsx
+++ src/pages/display/index.tsx
@@ -53,6 +53,7 @@ function DisplayPage() {
       <DisplayWrapper>
         <LeftGroup>
           <Accordion
+            closeAll={true}
             data={[
               {
                 title: '메인배너',
